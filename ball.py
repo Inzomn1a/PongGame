0fbdb60ec1419d6841e56df1967de652f87d997c
inzomnia <inzomnia7@gmail.com>
2022-02-18 13:40:07 +0100

    Added ball collision logic for top and bottom

--- ball.py
+++ ball.py
@@ -3,10 +3,18 @@ from turtle import Turtle
 
 class Ball:
     def __init__(self):
-        self.ball = Turtle("circle")
+        self.ball = Turtle("square")
         self.ball.color("white")
         self.ball.penup()
-        self.ball.setheading(40)  # initial moveDirection(degrees) of ball on beginPlay
+        self.ball_speed_x = 1
+        self.ball_speed_y = 1
 
     def move_ball(self):
-        self.ball.forward(5)  # Speed of ball
+        ball_xcor = self.ball.xcor() + self.ball_speed_x
+        ball_ycor = self.ball.ycor() + self.ball_speed_y
+        self.ball.setposition(ball_xcor, ball_ycor)
+        return self.ball.ycor()
+
+    def ball_bounce_top_bottom(self):
+        self.ball_speed_y *= -1
+
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("Pong")
-time.sleep(0.1)  # short delay after screen init and before game being rendered
+
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+time.sleep(0.1)  # after game setup: short delay before game starts
 
 # GameLoop
 screen.tracer(0)  # allows the use of 'screen.update()' function
@@ -26,7 +27,9 @@ while game_is_on:
     screen.onkey(l_paddle.move_up, "w")
     screen.onkey(l_paddle.move_down, "s")
     ball.move_ball()
-
+    if ball.move_ball() == 290 or ball.move_ball() == -290:  # check for top or bottom ball collision
+        print("yCollision")
+        ball.ball_bounce_top_bottom()
 
 
 
