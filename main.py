9c7373e53a962f17f7a26e144664627bc4ed8215
inzomnia <inzomnia7@gmail.com>
2022-02-18 12:32:49 +0100

    Moved Paddle into Class

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
+import time
 
 screen = Screen()
+
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("Pong")
-
-
-right_paddle = Turtle("square")
-right_paddle.color("white")
-right_paddle.shapesize(1, 5)
-right_paddle.setheading(90)
-right_paddle.penup()
-right_paddle.setposition(350, 0)
-
-
-def move_up():
-    right_paddle.forward(20)
-
-
-def move_down():
-    right_paddle.backward(20)
+time.sleep(0.5)  # short delay after screen init and before game being rendered
+paddle = Paddle()
 
 
 screen.tracer(0)  # allows the use of 'screen.update()' function
@@ -28,8 +17,8 @@ game_is_on = True
 while game_is_on:
     screen.update()
     screen.listen()
-    screen.onkey(move_up, "Up")
-    screen.onkey(move_down, "Down")
+    screen.onkey(paddle.move_up, "Up")
+    screen.onkey(paddle.move_down, "Down")
 
 
 
