b3b64e285d0c7d0bced4c2f3008452a30e5b0690
inzomnia <inzomnia7@gmail.com>
2022-02-18 12:53:32 +0100

    Added second paddle with separate controls using paddle class

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("Pong")
-time.sleep(0.5)  # short delay after screen init and before game being rendered
-paddle = Paddle()
+time.sleep(0.1)  # short delay after screen init and before game being rendered
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-screen.tracer(0)  # allows the use of 'screen.update()' function
 # GameLoop
+screen.tracer(0)  # allows the use of 'screen.update()' function
 game_is_on = True
 while game_is_on:
     screen.update()
     screen.listen()
-    screen.onkey(paddle.move_up, "Up")
-    screen.onkey(paddle.move_down, "Down")
+    screen.onkey(r_paddle.move_up, "Up")
+    screen.onkey(r_paddle.move_down, "Down")
+    screen.onkey(l_paddle.move_up, "w")
+    screen.onkey(l_paddle.move_down, "s")
 
 
 
--- paddle.py
+++ paddle.py
@@ -1,14 +1,15 @@
 from turtle import Turtle
 
 
-class Paddle:
-    def __init__(self):
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
         self.paddle = Turtle("square")
         self.paddle.color("white")
         self.paddle.shapesize(1, 5)
         self.paddle.setheading(90)
         self.paddle.penup()
-        self.paddle.setposition(350, 0)
+        self.paddle.goto(position)
 
     def move_up(self):
         self.paddle.forward(20)
