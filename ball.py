27c5efcc68db7b20f44cebf0cb216d282e6f8487
inzomnia <inzomnia7@gmail.com>
2022-02-18 15:38:59 +0100

    Added Logic for ballCollision with paddles

--- ball.py
+++ ball.py
@@ -1,20 +1,24 @@
 from turtle import Turtle
 
 
-class Ball:
+class Ball(Turtle):
     def __init__(self):
-        self.ball = Turtle("square")
-        self.ball.color("white")
-        self.ball.penup()
-        self.x_move = 0.5  # Higher value increases ball speed
-        self.y_move = 0.5  # Higher value increases ball speed
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.x_move = 1  # Higher value increae ball speed (steps of 0.25)
+        self.y_move = 1  # Higher value increae ball speed (steps of 0.25)
 
     def move_ball(self):
-        ball_xcor = self.ball.xcor() + self.x_move
-        ball_ycor = self.ball.ycor() + self.y_move
-        self.ball.setposition(ball_xcor, ball_ycor)
-        return self.ball.ycor()
+        ball_xcor = self.xcor() + self.x_move
+        ball_ycor = self.ycor() + self.y_move
+        self.setposition(ball_xcor, ball_ycor)
+        #return self.ycor()
 
     def ball_bounce_top_bottom(self):
         self.y_move *= -1
+
+    def ball_bounce_paddle(self):
+        self.x_move *= -1
 
--- paddle.py
+++ paddle.py
@@ -1,17 +1,19 @@
 from turtle import Turtle
+import time
 
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, position, color):
-        self.paddle = Turtle("square")
-        self.paddle.color(color)
-        self.paddle.shapesize(1, 5)
-        self.paddle.setheading(90)
-        self.paddle.penup()
-        self.paddle.goto(position)
+        super().__init__()
+        self.shape("square")
+        self.color(color)
+        self.shapesize(1, 5)
+        self.setheading(90)
+        self.penup()
+        self.goto(position)
 
     def move_up(self):
-        self.paddle.forward(20)
+        self.forward(20)
 
     def move_down(self):
-        self.paddle.backward(20)
+        self.backward(20)
